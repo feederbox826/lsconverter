63e318410090d93964f4943294794fdff7461340
feederbox826 <feederbox826@users.noreply.github.com>
2024-08-25 20:23:37 -0400

    add choice following, add type

--- convert.js
+++ convert.js
@@ -29,44 +29,44 @@ function convert(text) {
   // Reads game.xml file
   var xmlDoc = $.parseHTML(text);
   var gamePages = [];
-
+  
   $(xmlDoc).each(function (i, elem) {
-      var page = {};
-      page.id = $(this).attr('id');
-      page.stype = $(this).attr('stype');
-      page.type = $(this).attr('type');
-      page.resume = $(this).attr('resume');
-      page.wcount = $(this).attr('wcount');
-      page.wcounta = $(this).attr('wcounta');
-      page.wcountb = $(this).attr('wcountb');
-      page.qtext = $(this).attr('qtext');
-      page.video = $(this).attr('video');
-
-      for (i = 1; i < page.wcount; i++) {
-          page["id" + i] = $(this).attr("id" + i);
-          page["photo" + i] = $(this).attr("photo" + i);
-          page["ctext" + i] = $(this).attr("ctext" + i);
-      }
-      for (i = 1; i <= page.wcounta; i++) {
-          page["aid" + i] = $(this).attr("aid" + i);
-          page["aphoto" + i] = $(this).attr("aphoto" + i);
-      }
-      for (i = 1; i <= page.wcountb; i++) {
-          page["bid" + i] = $(this).attr("bid" + i);
-          page["bphoto" + i] = $(this).attr("bphoto" + i);
-      }
-
-      gamePages[page.id] = page;
+    var page = {};
+    page.id = $(this).attr('id');
+    page.stype = $(this).attr('stype');
+    page.type = $(this).attr('type');
+    page.resume = $(this).attr('resume');
+    page.wcount = $(this).attr('wcount');
+    page.wcounta = $(this).attr('wcounta');
+    page.wcountb = $(this).attr('wcountb');
+    page.qtext = $(this).attr('qtext');
+    page.video = $(this).attr('video');
+    
+    for (i = 1; i < page.wcount; i++) {
+      page["id" + i] = $(this).attr("id" + i);
+      page["photo" + i] = $(this).attr("photo" + i);
+      page["ctext" + i] = $(this).attr("ctext" + i);
+    }
+    for (i = 1; i <= page.wcounta; i++) {
+      page["aid" + i] = $(this).attr("aid" + i);
+      page["aphoto" + i] = $(this).attr("aphoto" + i);
+    }
+    for (i = 1; i <= page.wcountb; i++) {
+      page["bid" + i] = $(this).attr("bid" + i);
+      page["bphoto" + i] = $(this).attr("bphoto" + i);
+    }
+    
+    gamePages[page.id] = page;
   });
   return gamePages;
 }
 
 const parsed = convert(xmlString);
-//console.log(parsed)
 
 const gameId = 85500
+const mapped = []
 const fakeURL = (path) => `http://localhost:9999/custom/serve/games/${gameId}/images/${path}`
-const findId = (id) => parsed.find(page => page.id === id)
+const findParsed = (id) => parsed.find(page => page.id === id)
 const randomActionID = () => "aid"+crypto.randomBytes(4).toString("hex")
 
 const mapAction = (bidElem, i) => ({
@@ -75,7 +75,7 @@ const mapAction = (bidElem, i) => ({
     content: fakeURL(bidElem["aphoto" + i])+".jpg"
   },
   video: {
-    resolved_content: fakeURL(findId(bidElem["aid" + i]).video)
+    resolved_content: fakeURL(findParsed(bidElem["aid" + i]).video)
   }
 })
 
@@ -89,6 +89,7 @@ for (let i = 0; i < parsed.length; i++) {
   const page = parsed[i];
   const basePage = {
     id: page.id,
+    type: page.type,
     title: page.qtext,
     resource: {
       resolved_content: fakeURL(page.video)
@@ -107,10 +108,9 @@ for (let i = 0; i < parsed.length; i++) {
     }
   }
   // bid - fragment
-  // add on to first fragment
   for (let b = 1; b <= page.wcountb; b++) {
-    const bidElem = findId(page["bid" + b])
-    const fragid = "frag"+page["bid" + b]
+    const fragid = page["bid" + b]
+    const bidElem = findParsed(fragid)
     const fragment = {
       id: fragid,
       photo: {
@@ -121,26 +121,40 @@ for (let i = 0; i < parsed.length; i++) {
       },
       actions: []
     }
-    // if bid !== curid, fetch actions
-    if ("frag"+page.id !== fragid) {
-      for (let a = 1; a <= bidElem.wcounta; a++) {
-        fragment.actions.push(mapAction(bidElem, a))
-      }
+    // fetch actions
+    for (let a = 1; a <= bidElem.wcounta; a++) {
+      fragment.actions.push(mapAction(bidElem, a))
     }
     basePage.fragments.push(fragment)
   }
-  // aid - actions
-  for (let a = 1; a <= page.wcounta; a++) {
-    // add to first fragment
-    basePage.fragments[0].actions.push(mapAction(page, a))
-  }
+
   // clean fragments if dne
   if (basePage.fragments.length === 0) {
     delete basePage.fragments
   } else { // if has fragments, set resource id to be empty
     delete basePage.resource.resolved_content
   }
+  
+  // add to mapped
+  mapped.push(basePage)
+}
 
-  // write to file
+const findMapped = (id) => mapped.find(page => page.id === id)
+
+// cleanup run
+for (const basePage of mapped) {
+  // choice cleanup
+  // while id is included in fragment id, override with the last one
+  curPage = basePage
+  while ((curPage.fragments || []).some(frag => frag.id === curPage.id) && curPage.choices.length == 1 && curPage.choices[0].id !== curPage.id) {
+    // folow the id of last frag
+    const nextIdx = curPage.choices[0].id
+    curPage = findMapped(nextIdx)
+  }
+  // if last page is not the same as the current page, override choices
+  if (curPage.id !== basePage.id) {
+    basePage.choices = curPage.choices
+  }
+  // end cleanup loop
   fs.writeFileSync(`./serve/games/85500/choices/${basePage.id}.json`, JSON.stringify(basePage, null, 2))
 }
